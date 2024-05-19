29746e1e533e1d741c3cf6fc59725d47b22d52ba
jota santos <gestald118@email.com>
2024-05-19 18:38:24 -0400

    Configuração

---
+++ src/main/java/com/jotasantos/springbootcrud/springbootcrud/core/config/ApiPath.java
@@ -0,0 +1,6 @@
+package com.jotasantos.springbootcrud.springbootcrud.core.config;
+
+public class ApiPath {
+    public static final String ROOT = "/api/v1";
+    public static final String PRODUTOS = "/api/v1/produtos";
+}
--- src/main/java/com/jotasantos/springbootcrud/springbootcrud/utils/DataUtil.java
+++ src/main/java/com/jotasantos/springbootcrud/springbootcrud/utils/DataUtil.java
@@ -1,4 +1,13 @@
 package com.jotasantos.springbootcrud.springbootcrud.utils;
 
+import java.time.LocalDateTime;
+
 public class DataUtil {
+
+    public static String getDataAtualDMYHMS() {
+        LocalDateTime now = LocalDateTime.now();
+        return now.format(DateFormats.DMYHMS);
+    }
+
+
 }
